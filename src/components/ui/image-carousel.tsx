6947d9ad080ce0a156a177b740d5b6b205ec0dc4
galuhsatria <galuhsatria219@gmail.com>
2024-10-20 20:39:43 +0800

    feat: complate peta page

---
+++ src/components/ui/image-carousel.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { GoDotFill } from 'react-icons/go';
+import { Carousel, CarouselContent, CarouselItem, type CarouselApi } from '@/components/ui/carousel';
+
+export default function CarouselWrapper({ images }: { images: string[] }) {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  return (
+    <div className="relative">
+      <Carousel setApi={setApi}>
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="relative">
+                <img src={image} className="rounded-md w-full h-full object-cover" />
+                <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-black to-transparent rounded-b-md opacity-60"></div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      <div className="absolute inset-x-0 bottom-4 flex justify-center">
+        {Array.from({ length: count }).map((_, dotIndex) => (
+          <GoDotFill key={dotIndex} className={`text-lg cursor-pointer ${current === dotIndex + 1 ? 'text-green-500' : 'text-gray-400 opacity-50'}`} onClick={() => api?.scrollTo(dotIndex)} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/routes/peta.tsx
+++ src/routes/peta.tsx
@@ -1,11 +1,76 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from '@/components/ui/drawer';
+import CarouselWrapper from '@/components/ui/image-carousel';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { tempatWisata } from '@/data/dummyData';
+import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { FaCheckCircle } from 'react-icons/fa';
 
-export default function Peta() {
+export default function PetaTempatWisata() {
+  const defaultPosition: [number, number] = [-8.6924989, 116.1063038];
+  const [selectedTempat, setSelectedTempat] = useState<any>(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleMarkerClick = (tempat: any) => {
+    setSelectedTempat(tempat);
+    setDrawerOpen(true);
+  };
+
   return (
     <>
       <Helmet>
-        <title>Desa Wisata Kebon Ayu | Peta</title>
+        <title>Tempat Wisata | Peta</title>
       </Helmet>
+
+      <div>
+        <MapContainer center={defaultPosition} zoom={15} style={{ height: '100vh', width: '100%', zIndex: 1 }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          {tempatWisata.map((tempat, index) => (
+            <Marker
+              key={index}
+              position={[parseFloat(tempat.lokasi.lat), parseFloat(tempat.lokasi.long)]}
+              eventHandlers={{
+                click: () => handleMarkerClick(tempat),
+              }}
+            ></Marker>
+          ))}
+        </MapContainer>
+
+        <Drawer open={drawerOpen} onOpenChange={setDrawerOpen}>
+          <DrawerContent>
+            <DrawerHeader>
+              <div className="max-w-xl mx-auto px-4 text-left">
+                <DrawerTitle className="px-4">Informasi Wisata</DrawerTitle>
+                <ScrollArea className="h-[30rem] max-w-4xl mt-6 rounded-md px-4">
+                  <div className="">
+                    <div className="w-full">
+                      <CarouselWrapper images={selectedTempat?.image} />
+                    </div>
+                    <p className="mt-4 text-xs bg-green-500 px-4 py-1 rounded-full text-white w-max">{selectedTempat?.kategori}</p>
+                    <p className='text-xl font-bold mt-5  '>{selectedTempat?.nama}</p>
+
+                    <p className="mt-2 text-justify text-sm">{selectedTempat?.deskripsiPanjang}</p>
+                    <div>
+                      <p className="font-bold text-lg mt-4">Fasilitas</p>
+                      <div className="flex flex-wrap gap-6 mt-4">
+                        {selectedTempat?.fasilitas.map((fasilitas: string, index: number) => (
+                          <p key={index} className="flex gap-2 items-center">
+                            <FaCheckCircle className="text-green-500" /> {fasilitas}
+                          </p>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </ScrollArea>
+              </div>
+            </DrawerHeader>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </>
   );
 }
