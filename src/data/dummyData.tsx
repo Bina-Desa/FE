ecc5a58cb0f5180b6dff279213145f362d68b0e3
galuhsatria <galuhsatria219@gmail.com>
2024-10-20 11:05:19 +0800

    feat:dummy data

--- src/data/dummyData.tsx
+++ src/data/dummyData.tsx
@@ -11,25 +11,25 @@ export const tempatWisata = [
     image: ['https://jadesta.kemenparekraf.go.id/imgpost/115986.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115989.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115990.jpg'],
   },
   {
-    nama: 'Wisata Alam Jembatan Gantung',
+    nama: 'Wisata Kuliner Traditional',
     lokasi: {
-      lat: '-8.6924989',
-      long: '116.1063038',
+      lat: '-8.6929835',
+      long: '116.1020709',
       gmaps:
-        'https://www.google.com/maps/place/Jembatan+Gantung+Kebun+Ayu+Gerung/@-8.6924989,116.1063038,17z/data=!3m1!4b1!4m6!3m5!1s0x2dcdbd0948b54709:0x3825cef3b92029e3!8m2!3d-8.6925042!4d116.1088787!16s%2Fg%2F11fn51mvr_?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D',
+        'https://www.google.com/maps/place/Wisata+Kuliner+Kebon+Ayu/@-8.6929835,116.1020709,17z/data=!3m1!4b1!4m6!3m5!1s0x2dcdbddcf92518ef:0xc19d8aa1235a3045!8m2!3d-8.6929888!4d116.1046458!16s%2Fg%2F11p5kz19zy?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D',
     },
-    deskripsi: 'Nikmati pengalaman melewati jembatan gantung dengan pemadangan yang indah',
-    image: ['https://jadesta.kemenparekraf.go.id/imgpost/115986.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115989.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115990.jpg'],
+    deskripsi: 'Rasakan dan Nikmati berbagai macam jenis kuliner traditional yang pastinya menarik untuk dicoba',
+    image: ['https://jadesta.kemenparekraf.go.id/imgpost/114739.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/116141.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/116144.jpg'],
   },
   {
-    nama: 'Wisata Alam Jembatan Gantung',
+    nama: 'Wisata Alam Kebun Bambu',
     lokasi: {
       lat: '-8.6924989',
       long: '116.1063038',
       gmaps:
         'https://www.google.com/maps/place/Jembatan+Gantung+Kebun+Ayu+Gerung/@-8.6924989,116.1063038,17z/data=!3m1!4b1!4m6!3m5!1s0x2dcdbd0948b54709:0x3825cef3b92029e3!8m2!3d-8.6925042!4d116.1088787!16s%2Fg%2F11fn51mvr_?entry=ttu&g_ep=EgoyMDI0MTAxNi4wIKXMDSoASAFQAw%3D%3D',
     },
-    deskripsi: 'Nikmati pengalaman melewati jembatan gantung dengan pemadangan yang indah',
-    image: ['https://jadesta.kemenparekraf.go.id/imgpost/115986.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115989.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115990.jpg'],
+    deskripsi: 'Jelajahi spot foto yang menarik dan indah dengan pemadangan pohon bambu',
+    image: ['https://jadesta.kemenparekraf.go.id/imgpost/115948.jpg', 'https://jadesta.kemenparekraf.go.id/imgpost/115956.jpg'],
   },
 ];
