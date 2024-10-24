9ccd41f670c9de7d1d3392949fb8b3257d3753d3
galuhsatria <galuhsatria219@gmail.com>
2024-10-24 18:39:43 +0800

    feat: page tempat wisata

--- src/routes/tempat-wisata.tsx
+++ src/routes/tempat-wisata.tsx
@@ -1,6 +1,44 @@
+import { tempatWisata } from '@/data/dummyData';
+import CarouselWrapper from '@/components/ui/image-carousel';
+import { IoLocationOutline } from 'react-icons/io5';
+
+interface WisataAlam {
+  nama: string;
+  lokasi: {
+    lat: string;
+    long: string;
+    gmaps: string;
+  };
+  deskripsiPendek: string;
+  image: string[];
+}
 
 export default function TempatWisata() {
   return (
-    <div>TempatWisata</div>
-  )
+    <div className="min-h-screen py-28">
+      <div className='text-center'>
+        <h1 className="text-center text-3xl font-bold">
+          Tempat <span className="text-green-500">Wisata</span>
+        </h1>
+        <p className='text-zinc-400'>Temukan berbagai tempat wisata yang menarik</p>
+      </div>
+
+      <div>
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 my-10 max-w-6xl mx-auto px-4">
+          {tempatWisata.map((wisata: WisataAlam, index: number) => (
+            <div key={index} className="bg-white shadow-md rounded-md p-3 pb-6 relative">
+              <CarouselWrapper images={wisata.image} />
+              <div className="">
+                <p className="font-bold text-lg mt-4">{wisata.nama}</p>
+                <p className="text-sm text-zinc-500 mb-4">{wisata.deskripsiPendek}</p>
+                <a href={wisata.lokasi.gmaps} className="bg-green-500 px-4 py-2 rounded-md text-white mt-4 text-sm hover:bg-green-600 flex items-center w-max gap-1" target="_blank" rel="noopener noreferrer">
+                  Lihat Maps <IoLocationOutline />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
