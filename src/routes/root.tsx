9d127a9f5f507971aabf09efc27643ed8c4c0975
galuhsatria <galuhsatria219@gmail.com>
2024-12-16 18:49:40 +0800

    add whatsapp

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,9 +3,11 @@ import Footer from '@/components/ui/footer';
 import Navbar from '@/components/ui/navbar';
 import ScrollToTop from '@/utils/scroll-top';
 import { Outlet, useLocation } from 'react-router-dom';
+import { IoLogoWhatsapp } from 'react-icons/io5';
 
 export default function Root() {
   const location = useLocation();
+
   return (
     <>
       <ScrollToTop />
@@ -18,6 +20,9 @@ export default function Root() {
         <Outlet />
       )}
       <Footer />
+      <a href="https://wa.me/6281234567890" target="_blank" rel="noopener noreferrer" className="fixed bottom-14 right-10 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition duration-300 border border-white cursor-pointer z-10">
+        <IoLogoWhatsapp size={24} />
+      </a>
     </>
   );
 }
