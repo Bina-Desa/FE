64bc4a6c075b89df0bc2f684645a398f4b691c95
galuhsatria <galuhsatria219@gmail.com>
2025-02-02 11:49:51 +0800

    delete import unused

--- src/routes/admin/wisata/update-wisata.tsx
+++ src/routes/admin/wisata/update-wisata.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { SearchControl } from '../components/search-place';
 import { toast } from 'sonner';
